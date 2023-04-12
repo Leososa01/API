0fa5b31fcb26e0a5a482191e2b1588b0f0e8be57
Leososa01 <124358126+Leososa01@users.noreply.github.com>
2023-04-12 02:33:47 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ValidacionController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-    
+//comentario
 });
 //ruta
 Route::get('form', [ValidacionController::class,'index']);
